9eb80483d66279fb7737357aa1b8d5694d7b6494
liza <lizashatuzhilkina@gmail.com>
2022-06-09 23:01:00 +0300

    ex

---
+++ 66/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+// a
+int start = GetDemension("Введите число от которого считать:");
+int end = GetDemension("Введите число до которого считать:");
+
+int GetDemension(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int Recursion(int start, int end)
+{
+    if (end == start)
+        return start;
+
+    Console.WriteLine($"start {start} ------ end {end}");
+    return start + Recursion(start + 1, end);// 4+5(recurcion(4+1'8))+ 6(recurcion(5+1,8))+7(recurcion(6+1,8))
+
+}
+var result = Recursion(start, end);
+Console.WriteLine(result);
---
+++ 68/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+
+*/
+int m = GetDemension("Введите неотрицательное число m:");
+int n = GetDemension("Введите неотрицательное число n:");
+
+
+int GetDemension(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+int Recursion(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Recursion(m - 1, 1);
+    }
+    else
+    {
+        return Recursion(m - 1, Recursion(m, n - 1));
+    }
+}
+int result = Recursion(m, n);
+Console.Write($"Функция Аккермана = {result}");
